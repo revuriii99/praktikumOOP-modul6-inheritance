ab6fe5fdcfedb7bfadae3334cec03a2973840d14
revuriii99 <reviwilerii@gmail.com>
2026-04-16 14:23:27 +0700

    Last commit

--- src/Manager.java
+++ src/Manager.java
@@ -1,7 +1,18 @@
-public class Manager {
+public class Manager extends Pekerja {
     private String departemen;
 
-    public Manager(String departemen){
-        this.departemen=departemen;
+    public Manager(String departemen, double gaji, int thn, int bln, int tgl, int anak, String nama, String nik, boolean jk, boolean m) {
+        super(gaji, thn, bln, tgl, anak, nama, nik, jk, m);
+        this.departemen = departemen;
+    }
+
+    @Override
+    public double getPendapatan() {
+        return super.getPendapatan() + (0.10 * getGaji());
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\ndepartemen : " + departemen;
     }
 }
--- src/Pekerja.java
+++ src/Pekerja.java
@@ -1,13 +1,37 @@
 import java.time.LocalDate;
+import java.time.Period;
 
-public class Pekerja {
+public class Pekerja extends Manusia {
     private double gaji;
     private LocalDate tahunMasuk;
     private int jumlahAnak;
 
-    public Pekerja(double gaji,LocalDate tahunMasuk,int jumlahAnak){
-        this.gaji=gaji;
-        this.tahunMasuk=tahunMasuk;
-        this.jumlahAnak=jumlahAnak;
+    public Pekerja(double gaji, int tahun, int bulan, int hari, int jumlahAnak, String nama, String nik, boolean jk, boolean m) {
+        super(nama, jk, nik, m);
+        this.gaji = gaji;
+        this.tahunMasuk = LocalDate.of(tahun, bulan, hari);
+        this.jumlahAnak = jumlahAnak;
     }
+
+    public double getBonus() {
+        int lamaBekerja = Period.between(tahunMasuk, LocalDate.now()).getYears();
+        if (lamaBekerja >= 0 && lamaBekerja <= 5) return 0.05 * gaji;
+        if (lamaBekerja > 5 && lamaBekerja <= 10) return 0.10 * gaji;
+        if (lamaBekerja > 10) return 0.15 * gaji;
+        return 0;
+    }
+
+    @Override
+    public double getPendapatan() {
+        return super.getPendapatan() + gaji + getBonus() + (jumlahAnak * 20.0);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\ntahun masuk : " + tahunMasuk.getDayOfMonth() + " " +
+                tahunMasuk.getMonthValue() + " " + tahunMasuk.getYear() +
+                "\njumlah anak : " + jumlahAnak + "\ngaji : " + gaji;
+    }
+
+    public double getGaji() { return gaji; }
 }
